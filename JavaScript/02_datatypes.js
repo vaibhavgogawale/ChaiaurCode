cbbd54fdfe884d8f90eb45cc78174bd789e1c8a6
Vaibhav Gogawale <vaibhavgogawale87801@gmail.com>
2024-12-18 09:26:55 +0000

    Today we Learn about Datatypes

---
+++ JavaScript/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"  // Treat all JS code as newer version
+
+// alert(1+2)       // We are using Node.js not browser
+
+console.log(3+
+    3);     // Code Readability should be High
+
+
+
+let name = "Vaibhav"
+let age = 23
+let isLogged_In = true
+
+// number   => 2 to power 53
+// bigint
+// string
+// boolean  =>  true/false
+// null     =>  standalone value
+// undefined    => 
+// symbol   =>  unique
+
+
+// object
+
+console.log(typeof age);        // use 'typeof' to find type of any variable
+
+console.log(typeof null);         // Type = object  
+console.log(typeof undefined);    // Type = undefined
